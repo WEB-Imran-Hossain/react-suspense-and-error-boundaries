f1cc4f151b4a67878aeecb5b491b99b397094153
WEB-Imran-Hossain <webimran2021@gmail.com>
2024-08-14 23:49:57 +0600

    suspenced methode for use data fetching

--- src/App.jsx
+++ src/App.jsx
@@ -9,13 +9,15 @@ function App() {
     setSelectedPostId(e.target.value);
   };
   return (
-    <div>
+    <div className="w-[80vw] mx-auto flex flex-col items-center mt-40">
       <h1 className="text-2xl font-bold">React Suspense and Error Boundaries</h1>
       <div>
-        <Suspense fallback={<h1>Loading...</h1>}>
+        <Suspense fallback={<h1>Loading posts...</h1>}>
           <PostSelector onSelectPost={handleSelectPost} />
         </Suspense>
-        {selectedPostId && <Comments postId={selectedPostId} />}
+        {selectedPostId && <Suspense fallback={<h1>Loading comments...</h1>}>
+                <Comments postId={selectedPostId} />
+              </Suspense>}
       </div>
     </div>
   );
--- src/components/Comments.jsx
+++ src/components/Comments.jsx
@@ -1,27 +1,23 @@
-import React from 'react';
-import use from '../hooks/use';
-import { fetchData } from '../utils/data';
+import React from "react";
+import use from "../hooks/use";
+import { fetchData } from "../utils/data";
 
 const Comments = ({ postId }) => {
-    const comments = use(
-        fetchData(
-            `https://jsonplaceholder.typicode.com/posts/${postId}/comments`
-        )
-    );
+  const comments = use(
+    fetchData(`https://jsonplaceholder.typicode.com/posts/${postId}/comments`)
+  );
 
-    return (
-        <div>
-            <h2>Comments</h2>
+  return (
+    <div>
+      <h2 className="text-xl font-bold my-5">Comments:</h2>
 
-            <div>
-                <ul>
-                    {comments.map((comment) => (
-                        <li key={comment.id}>{comment.name}</li>
-                    ))}
-                </ul>
-            </div>
-        </div>
-    );
-}
+      <ul>
+          {comments.map((comment) => (
+            <li key={comment?.id}>{comment?.name}</li>
+          ))}
+        </ul>
+      </div>
+  );
+};
 
 export default Comments;
