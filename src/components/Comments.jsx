edd34181be4e4e3d2b239dc0962388209f199db4
WEB-Imran-Hossain <webimran2021@gmail.com>
2024-08-14 23:14:53 +0600

    create coment components

--- src/components/Comments.jsx
+++ src/components/Comments.jsx
@@ -1,11 +1,27 @@
 import React from 'react';
+import use from '../hooks/use';
+import { fetchData } from '../utils/data';
 
-const Comments = () => {
+const Comments = ({ postId }) => {
+    const comments = use(
+        fetchData(
+            `https://jsonplaceholder.typicode.com/posts/${postId}/comments`
+        )
+    );
+
     return (
         <div>
-            Hello Comments
+            <h2>Comments</h2>
+
+            <div>
+                <ul>
+                    {comments.map((comment) => (
+                        <li key={comment.id}>{comment.name}</li>
+                    ))}
+                </ul>
+            </div>
         </div>
     );
-};
+}
 
 export default Comments;
